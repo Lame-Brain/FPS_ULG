45a1fc77e60ac40c8f4954b1f74e28cdf2d3a6f4
Ethan Roberts <theLameBrain@gmail.com>
2020-12-29 15:46:10 -0800

    Arranging things

---
+++ Assets/Scripts/MapGeometry.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeometry : MonoBehaviour
+{
+    public static MapGeometry MAP;
+
+    public GameObject[] model;
+    public Material[] texture;
+    public GameObject[] deco, toon;
+
+    void Awake()
+    {
+        if (MAP == null)
+        {
+            MAP = this;
+            DontDestroyOnLoad(MAP);
+        }
+        else Destroy(this);
+    }
+
+    public GameObject GetModel(string s)
+    {
+        GameObject target = null;
+        foreach (GameObject go in model) if (go.name == s) target = go;
+        return target;
+    }
+
+    public Material GetMat(string s)
+    {
+        Material target = null;
+        foreach (Material mat in texture) if (mat.name == s) target = mat;
+        return target;
+    }
+
+    public GameObject GetDeco(string s)
+    {
+        GameObject target = null;
+        foreach (GameObject go in deco) if (go.name == s) target = go;
+        return target;
+    }
+
+    public GameObject GetToon(string s)
+    {
+        GameObject target = null;
+        foreach (GameObject go in toon) if (go.name == s) target = go;
+        return target;
+    }
+}
--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     public TextAsset groundCSV, floor1CSV, floor2CSV, floor3CSV, blockingCSV, toonCSV, scriptCSV;
-    public GameObject boulderPF, brokePillar1PF, brokePillar2PF, cavePF, cubePF, hillPF, MountainPF, pillarPF;
 
     private int mapWidth = 32, mapLength = 32;
     private int[,] groundMap, f1Map, f2Map, f3Map, blockMap, toonMap, scriptMap;
@@ -34,7 +33,7 @@ public class MapManager : MonoBehaviour
     {
         //start by deleting existing level geometry
         GameObject[] levelGOs = GameObject.FindGameObjectsWithTag("level");
-        //foreach (GameObject go in levelGOs) Destroy(go);
+        foreach (GameObject go in levelGOs) Destroy(go);
 
         //load CSVs
         string[] row1 = groundCSV.text.Split(new char[] { '\n' });
@@ -66,6 +65,69 @@ public class MapManager : MonoBehaviour
         }
 
         //instantiate level geometry
+        GameObject ground;
+        for(int y = 0; y < mapLength; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                ground = null;
+                if ((groundMap[x,y] > 50 && groundMap[x,y] < 54) || groundMap[x,y] == 93) //Ground is liquid
+                {
+                    ground = Instantiate(MapGeometry.MAP.GetModel("LiquidCube"), new Vector3(x, -1, -y), MapGeometry.MAP.GetModel("LiquidCube").transform.rotation);
+                    if (groundMap[x, y] == 51) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Deep_Water"); //Deep Water
+                    if (groundMap[x, y] == 52) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Medium_Water"); //Medium Water
+                    if (groundMap[x, y] == 53) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Shallow_Water"); //Shallow Water
+                    if (groundMap[x, y] == 93) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Lava"); //Lava
+                }
+                if(groundMap[x,y] > 86 && groundMap[x,y] < 91) //Ground is a field
+                {
+                    ground = Instantiate(MapGeometry.MAP.GetModel("EnergyCube"), new Vector3(x, -1, -y), MapGeometry.MAP.GetModel("EnergyCube").transform.rotation);
+                    if (groundMap[x, y] == 87) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Poison_Field"); //Poison Field
+                    if (groundMap[x, y] == 88) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Energy_Field"); //Arcane Field
+                    if (groundMap[x, y] == 89) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Fire_Field"); //Fire Field
+                    if (groundMap[x, y] == 90) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Sleep_Field"); //Sleep Field
+                }
+                if (groundMap[x, y] == 59) //Ground is a Mountain
+                {
+                    ground = Instantiate(MapGeometry.MAP.GetModel("Mountain"), new Vector3(x, -1, -y), MapGeometry.MAP.GetModel("Mountain").transform.rotation);
+                }
+                if (groundMap[x, y] > 59 && groundMap[x, y] < 64) //Ground is a Mountain Cave
+                {
+                    ground = Instantiate(MapGeometry.MAP.GetModel("Cave"), new Vector3(x, -1, -y), MapGeometry.MAP.GetModel("Cave").transform.rotation);
+                    if (groundMap[x, y] == 60) ground.transform.rotation = Quaternion.Euler(-90f, 90f, 0);
+                    if (groundMap[x, y] == 61) ground.transform.rotation = Quaternion.Euler(-90f, 180f, 0);
+                    if (groundMap[x, y] == 62) ground.transform.rotation = Quaternion.Euler(-90f, 270f, 0);
+                    if (groundMap[x, y] == 63) ground.transform.rotation = Quaternion.Euler(-90f, 0, 0);
+                }
+
+                if(groundMap[x,y] == 58) //Hill
+                {
+                    ground = Instantiate(MapGeometry.MAP.GetModel("Hill"), new Vector3(x, -0.915f, -y), MapGeometry.MAP.GetModel("Hill").transform.rotation);
+                    ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Hill");
+                }
+
+                if (ground == null && groundMap[x,y] > 0) //Solid Ground
+                {
+                    ground = Instantiate(MapGeometry.MAP.GetModel("Cube"), new Vector3(x, -1, -y), MapGeometry.MAP.GetModel("Cube").transform.rotation);
+                    if (groundMap[x, y] == 54) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Swamp"); //Swamp
+                    if (groundMap[x, y] == 70) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Tile_Floor"); //Tile
+                    if (groundMap[x, y] == 80) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Stone_Wall"); //Stone Wall
+                    if (groundMap[x, y] == 81) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Locked_Door"); //Locked Door
+                    if (groundMap[x, y] == 82) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Door"); //Door
+                    if (groundMap[x, y] == 84) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Brick_Floor"); //Brick Floor
+                    if (groundMap[x, y] == 85) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Planks"); //Planks
+                    if (groundMap[x, y] == 90) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("White"); //White Block
+                    if (groundMap[x, y] == 92) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Secret_Passage"); //Secret Passage
+                    if (groundMap[x, y] == 94) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Brick_Wall"); //Brick Wall
+                    if (groundMap[x, y] == 95) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Black"); //Black
+                    if (groundMap[x, y] == 98) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Dirt"); //Dirt
+                    if (groundMap[x, y] == 99) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Grass"); //Grass
+                    if (groundMap[x, y] == 100) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Sand"); //Sand
+                    if (groundMap[x, y] == 101) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Snow"); //Snow
+                    if (groundMap[x, y] == 102) ground.GetComponent<MeshRenderer>().material = MapGeometry.MAP.GetMat("Stone"); //Stone
+                }
+            }
+        }
    }
      
 }
